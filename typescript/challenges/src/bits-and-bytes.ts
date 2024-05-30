737c2bab7db5219d073ed64e18dff770fe2f5886
Henning Tandberg <henning@cowork.no>
2024-05-30 14:55:19 +0200

    bitCount, byteCopy, byteZero and byteSet TypeScript

--- typescript/challenges/src/bits-and-bytes.ts
+++ typescript/challenges/src/bits-and-bytes.ts
@@ -1,33 +1,20 @@
-    // Original: int bitcount(unsigned x), "The C Programming Language", page 50
-export function bitCount(x: number): number {
-    let b: number;
-    let i: number;
-    let maxIterations = 32;
+// Original: int bitcount(unsigned x), "The C Programming Language", page 50
 
-    for (i = 0, b = 0; x != 0 && i < maxIterations; x >>= 1, i++) {
-        if ((x & 0x01) != 0)
-            b++;
-    }
+export const bitCount = (x: number): number => _bitCount(x, 0);
 
-    return b;
-}
+const _bitCount = (x: number, iterations: number): number =>
+    x == 0 || iterations == 32
+        ? 0
+        : _bitCount(x >> 1, ++iterations) + (x & 1);
 
-export function byteCopy(src: Uint8Array, dst: Uint8Array, size: number) : void {
-    if (size === 0)
-        return;
-
-    const n = Math.min(size, Math.min(src.length, dst.length));
-
-    for (let i = 0; i < n; i++)
-        dst[i] = src[i];
-}
+export const byteCopy = (src: Uint8Array, dst: Uint8Array, size: number) : void => src
+    .slice(0, Math.min(size, Math.min(src.length, dst.length)))
+    .forEach((value, i) => dst[i] = value);
 
 export function byteZero(a: Uint8Array, size: number) : void {
-    for (let i = 0; i < Math.min(size, a.length); i++)
-        a[i] = 0;
+    a.fill(0, 0, Math.min(size, a.length));
 }
 
 export function byteSet(a: Uint8Array, value: number, size: number) : void {
-    for (let i = 0; i < Math.min(size, a.length); i++)
-        a[i] = value;
+    a.fill(value, 0, Math.min(size, a.length));
 }
